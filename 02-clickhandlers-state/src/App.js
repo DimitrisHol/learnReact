98fcb4dec38ea761e04a62afd02be99b4d1b0989
Dimitris Hol <dimitrishol@protonmail.com>
2021-05-21 20:36:34 +0300

    first refactor of the statistics, removed state from stats

--- 02-clickhandlers-state/src/App.js
+++ 02-clickhandlers-state/src/App.js
@@ -2,6 +2,50 @@ import React, { useState } from 'react'
 import './App.css';
 
 
+const Statistics = ({good, neutral , bad}) => { 
+
+
+  const allReviews = good + neutral + bad
+  const averageScore = ((good - bad) / allReviews).toFixed(2)
+  const positivityPercentage = (good / allReviews).toFixed(2)
+
+
+  if (allReviews === 0) { 
+
+    return (<p>No Feedback Given</p>)
+  }
+
+  else {
+
+    return (
+
+      <div>
+        <h2>Statistics</h2>
+  
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+  
+        <p>All Reviews : {allReviews}</p>
+        <p>Average  : {averageScore}</p>
+        <p>Positive Percentage : {(positivityPercentage * 100)}%</p>
+        
+      </div>
+    )
+
+  }
+
+  
+
+
+}
+
+
+
+
+
+
+
 const App = () => {
 
   // Creating the state for the reviews.
@@ -10,37 +54,20 @@ const App = () => {
   const [neutral , setNeutral] = useState(0)
   const [bad , setBad] = useState(0)
 
-  // Statistics State
-
-  const [all , setAll] = useState(0)
-  const [average , setAverage] = useState(0)
-  const [positive , setPositive] = useState(0)
-
+  
   const clickHandlerGood = () =>  {
     setGood(good + 1)
-    setAll(all + 1)
-    setAverage(average + 1)
-    setPositive(good / all)
-    
   }
   const clickHandlerNeutral = () =>  {
     setNeutral(neutral + 1)
-    setAll(all + 1)
-    setPositive(good / all)
   }
   const clickHandlerBad = () =>  {
     setBad(bad + 1)
-    setAll(all + 1)
-    setAverage(average -1)
-    setPositive(good / all)
   }
-  
-
 
   return (
 
     <>
-
     <div>
 
       <h2>give feedback</h2>
@@ -49,21 +76,9 @@ const App = () => {
       <button onClick = {clickHandlerBad}>Bad</button>
     </div>
 
-    <div>
-      <h2>Statistics</h2>
+    <Statistics good = {good} neutral = {neutral} bad = {bad} />
 
-
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-
-      <p>All Reviews : {all}</p>
-      <p>Average  : {(average/all).toFixed(2)}</p>
-      <p>Positive Percentage : {(positive * 100).toFixed(2)}%</p>
-      
-
-
-    </div>
+    
 
     </>
 
