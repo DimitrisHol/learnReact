fd96f7b2983e504c782ea7b54201103cc54e776f
Dimitris Hol <dimitrishol@protonmail.com>
2021-05-21 20:48:15 +0300

    second refactor, Statistic , and Button components

--- 02-clickhandlers-state/src/App.js
+++ 02-clickhandlers-state/src/App.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import './App.css';
 
 
+const Statistic = ({text , value }) => <p>{text} : {value}</p>
+const Button = ({text , onClickMethod}) => <button onClick = {onClickMethod}>{text}</button>
+
 const Statistics = ({good, neutral , bad}) => { 
-
-
   const allReviews = good + neutral + bad
   const averageScore = ((good - bad) / allReviews).toFixed(2)
-  const positivityPercentage = (good / allReviews).toFixed(2)
-
+  const positivityPercentage = ((good / allReviews).toFixed(2)* 100) + "%"
 
   if (allReviews === 0) { 
 
@@ -16,54 +16,31 @@ const Statistics = ({good, neutral , bad}) => {
   }
 
   else {
-
     return (
-
       <div>
         <h2>Statistics</h2>
-  
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
-  
-        <p>All Reviews : {allReviews}</p>
-        <p>Average  : {averageScore}</p>
-        <p>Positive Percentage : {(positivityPercentage * 100)}%</p>
-        
+        <Statistic text = "Good" value = {good}/>
+        <Statistic text = "Neutral" value = {neutral}/>
+        <Statistic text = "Bad" value = {bad}/>
+        <Statistic text = "Total Reviews" value = {allReviews}/>
+        <Statistic text = "Average" value = {averageScore}/>
+        <Statistic text = "Positive Percentage" value = {positivityPercentage}/>
       </div>
     )
-
   }
-
-  
-
-
 }
-
-
-
-
-
-
 
 const App = () => {
 
-  // Creating the state for the reviews.
-
+  // State for the clickHandlers
   const [good , setGood] = useState(0)
   const [neutral , setNeutral] = useState(0)
   const [bad , setBad] = useState(0)
 
   
-  const clickHandlerGood = () =>  {
-    setGood(good + 1)
-  }
-  const clickHandlerNeutral = () =>  {
-    setNeutral(neutral + 1)
-  }
-  const clickHandlerBad = () =>  {
-    setBad(bad + 1)
-  }
+  const clickHandlerGood = () =>  setGood(good + 1)
+  const clickHandlerNeutral = () => setNeutral(neutral + 1)
+  const clickHandlerBad = () => setBad(bad + 1)
 
   return (
 
@@ -71,22 +48,14 @@ const App = () => {
     <div>
 
       <h2>give feedback</h2>
-      <button onClick = {clickHandlerGood}>Good</button>
-      <button onClick = {clickHandlerNeutral}>Neutral</button>
-      <button onClick = {clickHandlerBad}>Bad</button>
+      <Button onClickMethod = {clickHandlerGood} text = "Good"></Button>
+      <Button onClickMethod = {clickHandlerNeutral} text = "Neutral"></Button>
+      <Button onClickMethod = {clickHandlerBad} text = "Bad"></Button>
     </div>
 
     <Statistics good = {good} neutral = {neutral} bad = {bad} />
-
-    
-
     </>
-
-    
   )
 
-
-
 }
-
 export default App;
